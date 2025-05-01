41213b3af1336e0d0aefa1c7d3b559917d824bc3
hw <lhy3899@gmail.com>
2025-05-01 21:54:57 +0900

    스프링 기본 AppConfig 사용법으로 수정

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,16 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        /*AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();*/
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);//이름을 넣어줘서 찾아야 함. 이름,타입 순으로 입력.
 
         Member member = new Member(1L, "memberA", Grade.VIP); //Long 타입이니까 1L로 표기해야함.
         memberService.join(member);
