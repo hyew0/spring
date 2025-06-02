27bdc123d3d34d097643e57f6bd6836d23147069
hw <lhy3899@gmail.com>
2025-06-02 19:05:54 +0900

    로그인 추가

---
+++ login/src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,23 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final MemberRepository memberRepository;
+
+    /**
+     * @return null이면 로그인 실패
+     *
+     * 로그인의 핵심 비즈니스 로직은 회원을 조회한 다음에 파라미터로 넘어온 password와 비교해서 같으면 회원을 반환하고, 만약 password가 다르면 null 을 반환한다.
+     */
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
